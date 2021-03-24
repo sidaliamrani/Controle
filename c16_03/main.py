f3df96616bc69134b65409a86ef5bd99d2ed3f0f
sidaliamrani <77324898+sidaliamrani@users.noreply.github.com>
2021-03-24 19:59:58 +0100

    Update main.py

--- c16_03/main.py
+++ c16_03/main.py
@@ -9,7 +9,6 @@ note8 = ('eleve2', 'math', 14)
 
 notes = [note1, note2, note3, note4, note5, note6,note7,note8]
 
-
 #4 a)
 notes_elv1 = [note for note in notes if note[0] == 'eleve1']
 print(notes_elv1)
